ced3c04b4e97fb4ae228d68cd7e797ea882c149f
Dylan Wadkins <86080943+SteakSki@users.noreply.github.com>
2023-06-21 03:33:40 -0400

    Edited The Trees and Bushes In Start Scene

--- Share/Dylan/UI/title-play.gd
+++ Share/Dylan/UI/title-play.gd
@@ -12,4 +12,4 @@ func _process(_delta):
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://Dylan/Scenes/Start.tscn")
+	get_tree().change_scene_to_file("res://Assets/Scene_Levels/Start.tscn")
