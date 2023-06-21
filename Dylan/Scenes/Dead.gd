b07b6e6d6f6a3286986b061ddc712e80201311a0
Dylan Wadkins <86080943+SteakSki@users.noreply.github.com>
2023-06-20 21:31:44 -0400

    help

--- Dylan/Scenes/Dead.gd
+++ Dylan/Scenes/Dead.gd
@@ -4,14 +4,17 @@ extends Area3D
 @onready var anim_player = $DeadBody/AnimationPlayer
 @onready var collision = $CollisionShape3D
 @onready var scream = $AudioStreamPlayer3D
+@onready var help = $Help
 
 
 func _on_body_entered(body):
 	if body is Player:
-		deadbody.visible = true
-		scream.play()
+#		deadbody.visible = true
+#		scream.play()
+#		await get_tree().create_timer(0.1).timeout
+#		anim_player.play("mixamocom")
+#		await get_tree().create_timer(0.4).timeout
+		help.play()
 		await get_tree().create_timer(0.1).timeout
-		anim_player.play("mixamocom")
-		await get_tree().create_timer(0.4).timeout
 		collision.disabled = true
 
